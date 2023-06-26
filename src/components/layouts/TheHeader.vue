91d3460d6814efbef838a1fdd3b486aa89facf6d
Redwood <redwoodredwoodowski@gmail.com>
2023-06-26 22:52:19 +0200

    fix: use setup instead of mapActions

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,13 +1,27 @@
 <script lang=ts>
-import { mapState } from 'pinia';
+import { useGameStore } from '../../stores/GameStore';
 import { usePlayerStore } from '../../stores/PlayerStore';
 import SettingsIcon from '../icons/SettingsIcon.vue';
 
 export default {
   name: 'TheHeader',
+  emits: ['show-menu'],
+  setup() {
+    const playerStore = usePlayerStore();
+    const gameStore = useGameStore();
+    return { playerStore, gameStore };
+  },
   computed: {
-    ...mapState(usePlayerStore, ['players']),
     turnClass: () => (hasTurn: boolean) => (hasTurn ? 'turn' : ''),
+    players() {
+      return this.playerStore.players;
+    },
+    rounds() {
+      return this.gameStore.rounds;
+    },
+    moveCount() {
+      return this.gameStore.moveCount;
+    },
   },
   components: { SettingsIcon },
 };
@@ -21,38 +35,51 @@ export default {
           <span :class="`nickname ${turnClass(player.hasTurn)}`">{{ player.nickname }}</span>
           <span class="points">{{ player.points }}</span>
         </div>
+        <div class="details">
+          <div class="round">
+            <span>Round:</span>
+            <span>{{ rounds.current }}/{{ rounds.final }}</span>
+          </div>
+          <div class="move">
+            <span>Move:</span>
+            <span>{{ moveCount }}</span>
+          </div>
+          <button
+            type="button"
+            class="settings"
+            @click="$emit('show-menu')">
+            <SettingsIcon />
+          </button>
+        </div>
       </div>
     </div>
-    <button
-      type="button"
-      class="settings"
-      @click="">
-      <SettingsIcon />
-    </button>
   </header>
 </template>
 
 <style lang="scss" scoped>
 $font-color: rgb(221, 221, 221);
+$border-width: 3px;
+$green: rgb(57, 119, 57);
 
   header {
     display:flex;
-    width:100%;
-    justify-content: center;
-    padding:1rem;
-    align-items: center;
-    position: relative;
+    position: fixed;
+    z-index:1;
+    top:0;
+    padding:1em;
     .top {
       display:flex;
+      position:relative;
       background: rgb(18,18,18);
-      background: -moz-linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,0) 100%);
-      background: -webkit-linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,0) 100%);
-      background: linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,0) 100%);
+      background: -moz-linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,1) 100%);
+      background: -webkit-linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,1) 100%);
+      background: linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,1) 100%);
       min-width:500px;
+      white-space: nowrap;
       color:$font-color;
       justify-content: center;
       border-radius: 5em;
-      outline: 3px solid rgb(57, 119, 57);
+      border: $border-width solid $green;
       padding: 1rem;
       .players {
         display:flex;
@@ -85,17 +112,40 @@ $font-color: rgb(221, 221, 221);
           }
         }
       }
-    }
-    .settings {
-      position:absolute;
-      display:flex;
-      right:1em;
-      justify-content: center;
-      padding:0.6em;
-      align-items: center;
-      border-radius: 50%;
-      outline: 3px solid rgb(57, 119, 57);
-      aspect-ratio: 1/1;
+      .details {
+        display:flex;
+        position: absolute;
+        top:100%;
+        background: rgb(18,18,18);
+        background: -moz-linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,1) 100%);
+        background: -webkit-linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,1) 100%);
+        background: linear-gradient(145deg, rgba(18,18,18,1) 0%, rgba(64,64,64,1) 100%);
+        justify-content: center;
+        align-items: center;
+        padding:0.3em;
+        gap:1em;
+        font-size: smaller;
+        font-weight: bold;
+        text-align: center;
+        border: $border-width solid $green;
+        border-radius: 0 0 1em 1em;
+        & > * {
+          display:flex;
+          flex-direction: column;
+          background: none;
+          padding:0.3em;
+          border-radius: 0.4em;
+        }
+        .settings {
+          display:flex;
+          justify-content: center;
+          align-items: center;
+          transition: background .2s ease-in-out;
+          &:hover {
+            background: rgb(255,255,255, .2);
+          }
+        }
+      }
     }
   }
 
