9d81327a1272ea3016e8ac49435684f56171b699
Redwood <redwoodredwoodowski@gmail.com>
2023-06-26 22:51:31 +0200

    feat: init

---
+++ src/components/icons/ExpandDownIcon.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+  name: 'ExpandDownIcon',
+};
+</script>
+
+<template>
+  <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 7L12 13L18 7" stroke="#FFF" stroke-width="3" />
+    <path d="M17 18L7 18" stroke="#FFF" stroke-width="3" />
+  </svg>
+</template>
